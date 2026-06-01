3de372620e398a7af0346628ffc76371e2e7763a
ordexxa <triovation3@gmail.com>
2026-06-01 18:06:34 -0500

    Fix OpenTelemetry collector Docker configuration

--- observability/otel-collector/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-
-export PORT="${PORT:-8889}"
-
-sed "s/\${PORT}/${PORT}/g" /etc/otelcol-contrib/config.template.yaml > /tmp/otel-config.yaml
-
-exec /otelcol-contrib --config=/tmp/otel-config.yaml
